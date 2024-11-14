16495fe4131d99f9418966aa6789eda707d18e6a
Minju Kim <mnat713@naver.com>
2024-11-15 02:29:14 +0900

    [#5] family 앱 모델 pk, fk 수정

--- family/models.py
+++ family/models.py
@@ -3,9 +3,11 @@ from django.db import models
 # Create your models here.
 
 class FamilyInfo(models.Model):
+    family_id = models.BigAutoField()
     fam_num = models.PositiveIntegerField(default=1)
 
 class FamilyEmptyTime(models.Model):
-    family_id = models.ForeignKey(FamilyInfo, on_delete=models.CASCADE) 
+    family_empty_id = models.BigAutoField()
+    family = models.ForeignKey(FamilyInfo, on_delete=models.CASCADE) 
     family_empty_date = models.DateField()
     family_empty_min = models.PositiveIntegerField()
