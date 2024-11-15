e9196493364fdf4aeec281ebddc2a534f166cfcc
Minju Kim <mnat713@naver.com>
2024-11-15 10:29:39 +0900

    [#10] ad 앱 내 모델 생성

--- ads/models.py
+++ ads/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Place(models.Model):
+    place_id = models.BigAutoField(primary_key=True)
+    place_name = models.CharField(max_length=50, verbose_name='제휴업체 이름')
+    place_link = models.URLField(verbose_name='제휴업체 링크')
+    place_img = models.ImageField(verbose_name='제휴업체 사진', null=True, blank=True, upload_to='place_img')
+    place_min_time = models.PositiveIntegerField(verbose_name='제휴업체 최소 이용시간(분)')
+    
+    def __str__(self):
+        return self.place_name
