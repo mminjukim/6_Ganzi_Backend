042d4639d1747147b9c7020a7c45d38b92377979
Minju Kim <mnat713@naver.com>
2024-11-15 10:58:10 +0900

    [#10] place_img 필드 수정

--- ads/models.py
+++ ads/models.py
@@ -6,7 +6,7 @@ class Place(models.Model):
     place_id = models.BigAutoField(primary_key=True)
     place_name = models.CharField(max_length=50, verbose_name='제휴업체 이름')
     place_link = models.URLField(verbose_name='제휴업체 링크', max_length=500)
-    place_img = models.ImageField(verbose_name='제휴업체 사진', null=True, blank=True, upload_to='place_img%y%m%d')
+    place_img = models.ImageField(verbose_name='제휴업체 사진', null=True, blank=True, upload_to='place_img%Y%m%d/')
     place_min_time = models.PositiveIntegerField(verbose_name='제휴업체 최소 이용시간(분)')
 
     def __str__(self):
