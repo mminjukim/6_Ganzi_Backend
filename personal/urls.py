81ef200f4629df6f646ed362dbff02280e976eb3
largerejade <lsj31200@naver.com>
2024-11-15 13:09:13 +0900

    [#8] 메인페이지 api 구현

--- personal/urls.py
+++ personal/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
-from . import views
+from .views import *
 
 app_name = 'personal'
 
 urlpatterns = [
+    path('', HomeAPIView.as_view()),
+    #path('personal/one-word/'),
+    #path('personal/my-schedule/'),
+    #path('personal/register/'),
+    #path('personal/schedule/'),
+    #path('/personal/{personal_schedule_id}/edit/'),
+    #path('/personal/{personal_schedule_id}/delete'),
 ]
--- personal/views.py
+++ personal/views.py
@@ -1,3 +1,39 @@
+from datetime import timezone
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework import status
+from rest_framework.response import Response
+from sch_requests.models import Request, FamilySchedule
+from .models import FamilyMemo
+from accounts.models import User
+from ads.models import Place
+from django.db import models
+from .serializers import FamilyScheduleSerializer, FamilyMessageSerializer, AdSerializer
 
-# Create your views here.
+# 메인페이지 기능 구현
+class HomeAPIView(APIView):
+     def get(self, request):
+        user = request.user
+        accepted_requests = Request.objects.filter(
+            (models.Q(sent_user=user) | models.Q(target_user=user)) & models.Q(is_accepted=True)
+        )
+        schedules = FamilySchedule.objects.filter(fam_schedule_id__in=accepted_requests.values('fam_schedule_id'))
+        schedule_data = FamilyScheduleSerializer(schedules, many=True).data
+        
+        family_id = user.family
+        family_members = User.objects.filter(family = family_id)
+        family_memos = FamilyMemo.objects.filter(user__in=family_members, created_at__date=timezone.now().date())
+        
+        fam_message_data = FamilyMessageSerializer({
+            "family_id": family_id,
+            "members": family_memos
+        }).data
+        
+        ads = Place.objects.all()
+        ad_data = AdSerializer(ads, many=True).data
+
+        return Response({
+            "schedule": schedule_data,
+            "fam_message":fam_message_data,
+            "ad":ad_data
+        })
