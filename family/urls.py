5b8de4725de9248e5f87d6298dabb80fac1a93ad
Minju Kim <mnat713@naver.com>
2024-11-16 14:11:51 +0900

    [#12] family 앱 urls.py 수정

--- family/urls.py
+++ family/urls.py
@@ -4,6 +4,7 @@ from .views import *
 app_name = 'family'
 
 urlpatterns = [
+    path('calendar/<int:y>/<int:m>/<int:d>/', FamilyCalendarView.as_view()),
     path('incoming/', AllIncomingRequestsView.as_view()),
     path('incoming/<int:id>/', IncomingRequestView.as_view()),
     path('declined/', AllDeclinedRequestsView.as_view()),
