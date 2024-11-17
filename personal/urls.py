d6c9390b143ed0da5edfca13538769aec25653c0
largerejade <lsj31200@naver.com>
2024-11-17 18:40:35 +0900

    [#23] urls.py UPDATE

--- personal/urls.py
+++ personal/urls.py
@@ -5,8 +5,8 @@ app_name = 'personal'
 
 urlpatterns = [
     path('', HomeAPIView.as_view()),
-    path('personal/one-word/', OneWordAPIView.as_view),
-    path('personal/my-schedule/', ScheduleAPIView.as_view),
+    path('personal/one-word/', OneWordAPIView.as_view()),
+    path('personal/my-schedule/', ScheduleAPIView.as_view()),
     #path('personal/schedule/'),
     #path('/personal/{personal_schedule_id}/edit/'),
     #path('/personal/{personal_schedule_id}/delete'),
