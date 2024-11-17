6de13092788bfa5ab13d8f68ca4440163fa4cbe4
hyunerase <wldn0716@icloud.com>
2024-11-17 15:03:51 +0900

    [#21] 프로필 등록/수정 api 구현

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('kakao/login/', views.kakao_login, name='kakao_login'),
     path('kakao/callback/', views.kakao_callback, name='kakao_callback'),
     path('kakao/login/finish/', views.KakaoLogin.as_view(), name='kakao_login_todjango'),
+    path('profile/', views.ProfileAPIView.as_view()),
 ]
