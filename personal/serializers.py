0253e38e0f0b8a840526b96570786d857d15d326
largerejade <lsj31200@naver.com>
2024-11-17 11:57:41 +0900

    [#18] OneWordAPiView 기능 구현

--- personal/serializers.py
+++ personal/serializers.py
@@ -35,3 +35,8 @@ class AdSerializer(serializers.ModelSerializer):
     class Meta:
         model = Place
         fields = ['place_img']
+
+class OneWordSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = FamilyMemo
+        fields = ['content']
