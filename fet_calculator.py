a2945db89a4a85a33ae38df696a882730e94cced
Minju Kim <mnat713@naver.com>
2024-11-18 11:21:24 +0900

    [#28] FamilyEmptyTime calculator 생성

---
+++ fet_calculator.py
@@ -0,0 +1,54 @@
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "flan.settings")
+import django
+django.setup()
+
+from datetime import timedelta, datetime
+from django.utils import timezone
+from family.models import FamilyInfo, FamilyEmptyTime
+from accounts.models import User
+from personal.models import PersonalSchedule
+from sch_requests.models import FamilySchedule, Request
+
+
+def calc_family_empty_time():
+    all_families = FamilyInfo.objects.all()
+    today = datetime.now()
+    calc_start_date = datetime(today.year, today.month, today.day) + timedelta(days=1)
+    calc_end_date = datetime(today.year, today.month, today.day) + timedelta(days=7)
+    
+    for family in all_families:
+        fam_members = User.objects.filter(family=family)
+
+        user_schedules = PersonalSchedule.objects.filter(
+            user__in=fam_members, 
+            schedule_start_time__lt=calc_end_date,
+            schedule_end_time__gt=calc_start_date).order_by('schedule_start_time')
+        
+        # 스케줄이 겹치지 않는 빈 시간을 저장할 리스트
+        free_time_slots = []
+        last_end_time = calc_start_date
+
+        for schedule in user_schedules:
+            # 스케줄의 시작 시간이 이전 종료 시간보다 늦은 경우 빈 시간이 발생
+            if schedule.schedule_start_time.replace(tzinfo=None) > last_end_time:
+                free_time_slots.append((last_end_time, schedule.schedule_start_time.replace(tzinfo=None)))
+
+            # 이전 종료 시간을 현재 스케줄의 종료 시간으로 업데이트
+            last_end_time = max(last_end_time, schedule.schedule_end_time.replace(tzinfo=None))
+
+        # calc_end_date 이전의 빈 시간도 추가
+        if last_end_time < calc_end_date:
+            free_time_slots.append((last_end_time, calc_end_date))
+
+        requests = Request.objects.filter(target_user__in=fam_members, is_accepted=True)
+        fam_schedules = FamilySchedule.objects.filter(request__in=requests).distinct().order_by('schedule_start_time')
+        print(family.family_id, fam_schedules)
+
+        
+        
+            
+
+
+
+calc_family_empty_time()
