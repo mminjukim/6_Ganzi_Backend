931bc8f2b76c6ffb2c36f6d67fc1408ef0de3a75
hyunerase <wldn0716@icloud.com>
2024-11-23 10:44:55 +0900

    로그인

--- accounts/views.py
+++ accounts/views.py
@@ -18,7 +18,7 @@ from sch_requests.models import *
 from family.models import FamilyInfo
 from .serializers import UserSerializer, ProfileSerializer, SimpleUserSerializer
 
-BASE_URL = 'http://flan.klr.kr/'
+BASE_URL = 'https://flan.klr.kr/'
 KAKAO_CALLBACK_URI = BASE_URL + 'accounts/kakao/callback/'
 
 # JWT 토큰 생성 함수
