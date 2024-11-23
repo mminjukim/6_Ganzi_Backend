be79bcdd41f9386d9e87699ca6360d33e33506d7
hyunerase <wldn0716@icloud.com>
2024-11-23 11:40:22 +0900

    콜백 리다이렉트 변경

--- accounts/views.py
+++ accounts/views.py
@@ -99,12 +99,12 @@ def kakao_callback(request):
         user.username = f"kakao_{kakao_uid}"
         user.save()
 
-    jwt_access_token, jwt_refresh_token = create_jwt_token(user)
+    access_token, refresh_token = create_jwt_token(user)
 
-    frontend_url = f"http://localhost:5173/MyPage"
-    redirect_url = f"{frontend_url}?access_token={jwt_access_token}&refresh_token={jwt_refresh_token}"
-    return redirect(redirect_url)
-
+    return JsonResponse({
+        "access_token": access_token,
+        "refresh_token": refresh_token,
+    })
 
 def merge_social_account(user, social_account):
     try:
